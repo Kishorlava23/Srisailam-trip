3be711880ee527074fa1e0083ba9048d59df63af
Kishor Erupothu <109784829+Kishorlava23@users.noreply.github.com>
2026-09-21 09:26:10 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const sections = document.querySelectorAll('.section, .photo-break, .temple, .friends');
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, {threshold: 0.12});
-sections.forEach(section => observer.observe(section));
